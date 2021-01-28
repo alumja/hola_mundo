8c1bf4e8017533f73b79f0c5f614b032c632bfa0
alumja <jconesacursopoo@gmail.com>
2021-01-28 20:47:50 +0100

    main

---
+++ hola_mundo2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Escribir un programa que pregunte el nombre del usuario en la consoa y después de que el usuario
+por pantalla la cadena ¡Hola <nombre>!, done <nombre> es el nombre que el usuario haya introducido
+
+"""
+
+name = input("Introduce tu nombre: ")
+print("¡Hola " + name + "!")
